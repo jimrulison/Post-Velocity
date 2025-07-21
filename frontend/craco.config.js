e709bb1f552ebe7091f7eb20902a42a7f8bafd69
jimrulison <jimrulison@gmail.com>
2025-07-21 08:34:47 -0400

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,23 +1,3 @@
-info
-
-code
-Code
-
-preview
-Preview
-
-Deploy
-Deploy
-Robot
-Hey James, Quick input needed :
-Ah! I see the issue. Craco is looking for its configuration file, but we didn't upload the craco.config.js file during our manual process. We need to add the missing configuration files.
-
-🔧 MISSING CONFIGURATION FILES - LET'S ADD THEM:
-File 11: craco.config.js
-Go to your repository: https://github.com/jimrulison/Post-Velocity
-Click "Create new file"
-Filename: craco.config.js
-Copy and paste this content:
 const path = require('path');
 
 module.exports = {
