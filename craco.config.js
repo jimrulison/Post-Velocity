82b9d364202e7871e340ebaf33346a309e52ec99
jimrulison <jimrulison@gmail.com>
2025-07-21 07:53:56 -0400

    Create craco.config.js

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+  webpack: {
+    configure: (webpackConfig) => {
+      // Custom webpack configurations can go here
+      return webpackConfig;
+    },
+  },
+};
